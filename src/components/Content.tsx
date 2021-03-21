21564d861fecc416844a56a4d96bc3ddcf868fb5
saviobarbosa <saviiobarbosa@gmail.com>
2021-03-21 12:03:46 -0300

    Resolução do desafio 02

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
+import React, { useState } from "react";
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+  selectedGenre: string;
+}
+
+export function Content({ movies, selectedGenre}: MovieProps) {
   // Complete aqui
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+    </div>
+  )
 }
